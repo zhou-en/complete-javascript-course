a63a40ff907768f19ac8776330ea220374ae6885
En Zhou <zhouen.nathan@gmail.com>
2019-01-21 23:20:15 +0200

    Started section 3

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,7 +1,15 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+function calculateAge(year) {
+    console.log(2016 - year);
+}
 
+var retirement = function (year) {
+    console.log(65 - (2016 - year));
+}
+
+retirement(1990);
 
 
 
